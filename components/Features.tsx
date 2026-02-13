94cd58b9e3575518d0d7138cb64e8758bf6e21cc
Delmar Yap <yapdelmar@gmail.com>
2026-02-14 01:42:01 +0800

    Update components/Features.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- components/Features.tsx
+++ components/Features.tsx
@@ -12,7 +12,7 @@ const FeatureItem = ({title, icon, description}: FeatureItem) => {
     return (
         <li className="flex w-full flex-1 flex-col items-start">
             <div className="rounded-full p-4 lg:p-7 bg-green-50">
-                <Image src={icon} alt="map" width={28} height={28}/>
+                <Image src={icon} alt={`${title} icon`} width={28} height={28}/>
             </div>
             <h2 className="bold-20 lg:bold-32 mt-5 capitalize">
                 {title}
